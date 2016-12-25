0539974f9e17fb0e9f62341c90f0d137f3b97d32
Alexander Kramer <alex@tytoninae.de>
2016-12-25 10:55:46 +0100

    changed copyright

--- lib/Zwig/NodeHandler/IfHandler.php
+++ lib/Zwig/NodeHandler/IfHandler.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of Zwig.
  *
- * (c) Alexander Skrotzky
+ * (c) Alexander Kramer
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed
@@ -16,7 +16,6 @@ use Zwig\Compiler;
 use Zwig\Exception\NotImplementedException;
 use Zwig\Exception\UnknownStructureException;
 use Zwig\Sequence\Command;
-use Zwig\Sequence\Segment;
 
 
 /**
